0490e3830ba9bc7eeca79b18203c02ec2f74e63a
CheezItMan <chris@adadevelopersacademy.org>
2020-10-01 09:45:05 -0700

    solution

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,27 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 
-# Time Complexity: ?
-# Space Complexity: ?
 def max_sub_array(nums)
     return 0 if nums == nil
+    return nil if nums == []
     
-    raise NotImplementedError, "Method not implemented yet!"
+    curr_max = nums[0]
+    max_so_far = nums[0]
+
+    i = 1
+    until i == nums.length
+        if curr_max + nums[i] < nums[i]
+            curr_max = nums[i]
+        else
+            curr_max += nums[i]
+        end
+
+        if curr_max > max_so_far
+            max_so_far = curr_max
+        end
+        
+        i += 1
+    end
+
+    return max_so_far
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,18 @@
+# Time complexity: O(n)
+# Space Complexity: O(n)
 
-
-# Time complexity: ?
-# Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+  return "1" if num == 1
+  return "1 1" if num == 2
+  
+  s = [0, 1, 1]
+  result = "1 1"
+
+  for i in 3..num
+    s[i] = s[s[i - 1]] + s[i - s[i - 1]]
+    result << " #{s[i]}"
+  end
+
+  return result
 end
